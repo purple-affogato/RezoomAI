4c33d5ad172641d77d2e847eb90b49a054b71607
Carys Chiu <caryschiu@gmail.com>
2024-11-17 08:23:08 -0800

    backup updated with second txt and submit

--- frontend/src/Backup.jsx
+++ frontend/src/Backup.jsx
@@ -5,15 +5,14 @@ import EduEntry from './EduEntry'
 import ExperienceEntry from './ExperienceEntry'
 import { InputGroup, Form, Button,Badge } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import {Container, Nav, Navbar} from 'react-bootstrap';
 import {Row, Col} from 'react-bootstrap'
+import {Container, Nav, Navbar} from 'react-bootstrap';
 import mainLogo from '/RezoomAI-main-whitebg.svg'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Backup() {
     return(
         <div>
-
             <Navbar className="bg-body-tertiary" style={{backgroundColor: '#6383d4'}} >
                 <div className="col-md-1">
                     <Navbar.Brand href="#home">
@@ -30,7 +29,7 @@ function Backup() {
 
             <Form.Group className=""  controlID="formGridDescription" >
                 <InputGroup>
-                    <InputGroup.Text id="addon4">Description</InputGroup.Text>
+                    <InputGroup.Text id="addon1">Description</InputGroup.Text>
                     <Form.Control 
                         rows = "10"
                         type="text" 
@@ -40,9 +39,24 @@ function Backup() {
                 </InputGroup>
             </Form.Group>
 
-            <Link to="/joblisting">
-                <Button className="mb-3" variant="outline-secondary">Submit Resume</Button>
-            </Link>
+            <Form.Group className=""  controlID="formGridDescription" >
+                <InputGroup>
+                    <InputGroup.Text id="addon2">Job Listing</InputGroup.Text>
+                    <Form.Control 
+                        rows = "10"
+                        type="text" 
+                        as = "textarea"
+                        placeholder="Copy-paste your target job listing here."
+                    />
+                </InputGroup>
+            </Form.Group>
+
+            <Button className="mb-3" variant="outline-secondary" onClick={
+            // print link!!! print the link here!!!
+            console.log("yay")}> 
+                Submit Resume 
+            </Button>
+
         </div>
     );
 }
