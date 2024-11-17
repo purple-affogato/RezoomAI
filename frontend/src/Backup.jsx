d049930e2adf8c4a0016808b41c85ee14f98eb9b
Carys Chiu <caryschiu@gmail.com>
2024-11-17 08:09:25 -0800

    backup improved

--- frontend/src/Backup.jsx
+++ frontend/src/Backup.jsx
@@ -5,12 +5,29 @@ import EduEntry from './EduEntry'
 import ExperienceEntry from './ExperienceEntry'
 import { InputGroup, Form, Button,Badge } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import {Container, Nav, Navbar} from 'react-bootstrap';
 import {Row, Col} from 'react-bootstrap'
+import mainLogo from '/RezoomAI-main-whitebg.svg'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Backup() {
     return(
         <div>
+
+            <Navbar className="bg-body-tertiary" style={{backgroundColor: '#6383d4'}} >
+                <div className="col-md-1">
+                    <Navbar.Brand href="#home">
+                    <img src={mainLogo} className="logo RezoomAI" alt="RezoomAI main logo"/>
+                    </Navbar.Brand>
+                </div>
+                <div className="col-md-9"></div>
+                <div className="col-md-2">
+                    <Link to="/">
+                    <Button className="md-5" variant="outline-secondary">Sign Out</Button>
+                    </Link>
+                </div>
+            </Navbar>
+
             <Form.Group className=""  controlID="formGridDescription" >
                 <InputGroup>
                     <InputGroup.Text id="addon4">Description</InputGroup.Text>
