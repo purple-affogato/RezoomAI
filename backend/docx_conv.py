0652a6543e4278b7e2407ea8af809a83bdda595a
purple-affogato <ivy.yiting.zhuang@gmail.com>
2024-11-16 23:37:11 -0800

    Backend functions for sign-in, sign-out, and llm+docx

--- backend/docx_conv.py
+++ backend/docx_conv.py
@@ -3,15 +3,10 @@ from docx.enum.text import WD_ALIGN_PARAGRAPH, WD_LINE_SPACING
 from docx.shared import Pt
 
 # to be completed
-def generate_resume(name:str, contacts, input):
+def get_docx(text:str):
     doc = Document()
-    for i in range(1, 32):
-        para = doc.add_paragraph(str(i))
-        pformat = para.paragraph_format
-        pstyle = para.style
-        pformat.alignment = WD_ALIGN_PARAGRAPH.LEFT
-        pformat.line_spacing_rule = WD_LINE_SPACING.ONE_POINT_FIVE
-        pformat.space_after = Pt(0)
-        pstyle.font.name = "Arial"
-        pstyle.font.size = Pt(12)
+    para = doc.add_paragraph(text)
+    para.paragraph_format.line_spacing_rule = WD_LINE_SPACING.ONE_POINT_FIVE
+    para.style.font.size = Pt(12)
+    para.style.font.name = "Arial"
     doc.save("test.docx")
--- backend/server.py
+++ backend/server.py
@@ -1,30 +1,27 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
+from userlogin import sign_in, sign_out
+
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/user_login", methods=["POST"])
 def user_login():
     if request.method != "POST":
         return jsonify({'error':'Wrong HTTPS method'}), 400
     data = request.form
-    supabase_sign_in(email=data.get("email"), pw=data.get("password"))
-    return jsonify({'message':'u r so cool'}), 200
+    return jsonify(sign_in(email=data.get("email"), pw=data.get("password")))
 
-def supabase_sign_in(email: str, pw: str):
-    print(email, pw)
+@app.route("/user_logout", method=["POST"])
+def user_logout():
+    sign_out()
 
-@app.route("/user_signup", methods=["POST"])
-def user_signup():
+@app.route("/generate_resume", methods=["POST"])
+def generate_resume():
     if request.method != "POST":
         return jsonify({'error':'Wrong HTTPS method'}), 400
-    data = request.form
-
-    supabase_signup(email=data.get("email"), pw=data.get("password"))
-    return jsonify({'message':'good boy:3'}), 201
-
-def supabase_signup(email:str, pw:str):
     
-    print(email, pw)
     
 if __name__ == "__main__":
    app.run(port=5000, debug=True)
