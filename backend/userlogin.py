abe5dfa8e7e2939f7bc3b1a9ac00caaea4a38934
purple-affogato <ivy.yiting.zhuang@gmail.com>
2024-11-17 00:54:15 -0800

    Fixed up login

--- backend/userlogin.py
+++ backend/userlogin.py
@@ -7,11 +7,19 @@ url: str = os.getenv("SUPABASE_URL")
 key: str = os.getenv("SUPABASE_KEY")
 supabase: Client = create_client(url, key)
 
-def sign_in(email: str, pw: str):
-    response = supabase.auth.sign_in_with_password(
-        {"email": "izhuang@scu.edu", "password": "izhuang123"}
-    )
-    return response.user.id
+def sign_in(email: str, pw: str) ->dict:
+    dict_to_return = {}
+    dict_to_return['status'] = 200
+    try:
+        response = supabase.auth.sign_in_with_password(
+            {"email": email, "password": pw}
+        )
+        dict_to_return['uid'] = response.user.id
+        dict_to_return['session'] = response.session.access_token
+    except:
+       dict_to_return['status'] = 400
+    return dict_to_return
+        
 
 def sign_out():
     response = supabase.auth.sign_out()
