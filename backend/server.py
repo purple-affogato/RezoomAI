535f39bb7d21069aade24ddf8d6bcaa34b74c315
afung <afung@alexandria.local>
2024-11-16 19:27:24 -0800

    yay

--- backend/server.py
+++ backend/server.py
@@ -13,5 +13,18 @@ def user_login():
 def supabase_sign_in(email: str, pw: str):
     print(email, pw)
 
+@app.route("/user_signup", methods=["POST"])
+def user_signup():
+    if request.method != "POST":
+        return jsonify({'error':'Wrong HTTPS method'}), 400
+    data = request.form
+
+    supabase_signup(email=data.get("email"), pw=data.get("password"))
+    return jsonify({'message':'good boy:3'}), 201
+
+def supabase_signup(email:str, pw:str):
+    
+    print(email, pw)
+    
 if __name__ == "__main__":
    app.run(port=5000, debug=True)
