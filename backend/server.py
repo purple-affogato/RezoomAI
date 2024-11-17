a3ccc4ba82b21032ba09b9eabe68209a697a497a
afung <afung@alexandria.local>
2024-11-16 16:26:26 -0800

    table creation + writing stuff

--- backend/server.py
+++ backend/server.py
@@ -1,27 +1,18 @@
 from flask import Flask, request, jsonify
+from flask_pymongo import PyMongo
+import aws_controller
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Home"
+@app.route('/')
+def index():
+    return "home"
 
-# get post put delete
+@app.route('/get-items')
+def get_items():
+    return jsonify(aws_controller.get_items())
 
-@app.route("/get-user/<user_id>")
-def get_user(user_id):
-    user_data = {
-        "user_id": user_id,
-        "name": "John Doe",
-        "email": "john.doe@example.com"
-    }
+if __name__ == '__main__':
+    app.run()
 
-    extra = request.args.get("extra")
-    if extra:
-        user_data["extra"] = extra
-
-    return jsonify(user_data), 200
-
-if __name__ == "__main__":
-    app.run(debug=True)
 
